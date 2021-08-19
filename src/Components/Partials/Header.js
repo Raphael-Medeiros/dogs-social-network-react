9d3429fdfda62146719510c2a88f33f2fdbe3812
raphael-medeiros <raphaelmedeiros2605@gmail.com>
2021-08-19 13:13:51 -0700

    Finished header

--- src/Components/Partials/Header.js
+++ src/Components/Partials/Header.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+import styles from "./Header.module.css";
+import Logo from "./Logo";
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.container}>
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Logo />
+        <div className={styles.user}>
+          <div className={styles.userText}> Entrar / Criar</div>
+          <div className={styles.userIcon}></div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-            </div>
-        </header>
-    )
-}
-
-export default Header
+export default Header;
